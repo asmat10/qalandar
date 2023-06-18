c79f7b324df8ea8b3a1d1555cc88cfd5bcae9305
Asmat Simab <asmat@asmats-macbook-air.home>
2023-06-19 00:06:28 +0200

    Create Project

---
+++ src/main/java/com/qalandar/qalandar/QalandarApplication.java
@@ -0,0 +1,13 @@
+package com.qalandar.qalandar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QalandarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QalandarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qalandar/qalandar/QalandarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qalandar.qalandar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QalandarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
